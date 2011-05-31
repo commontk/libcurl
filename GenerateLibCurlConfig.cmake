f7bbc1f477edaaa5f11d257817cdca9c99136c66
Zach Mullen <zach.mullen@kitware.com>
2011-05-31 11:44:29 -0400

    ENH: Add libcurl config files for use as an external project

---
+++ GenerateLibCurlConfig.cmake
@@ -0,0 +1,19 @@
+CONFIGURE_FILE(
+  ${LIBCURL_SOURCE_DIR}/Uselibcurl.cmake.in
+  ${LIBCURL_BINARY_DIR}/Uselibcurl.cmake COPYONLY)
+
+# Library directory
+SET(libcurl_LIBRARY_DIRS_CONFIG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+
+# Include directories
+SET(libcurl_INCLUDE_DIRS_CONFIG 
+  ${LIBCURL_SOURCE_DIR}
+  )
+
+# Uselibcurl file
+SET(libcurl_USE_FILE_CONFIG ${LIBCURL_BINARY_DIR}/Uselibcurl.cmake)
+
+# Configure libcurlConfig.cmake
+CONFIGURE_FILE(
+  ${LIBCURL_SOURCE_DIR}/libcurlConfig.cmake.in
+  ${LIBCURL_BINARY_DIR}/libcurlConfig.cmake @ONLY)
